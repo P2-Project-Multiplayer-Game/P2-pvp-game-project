e2203a36e6ff52cfb2b35d2f3d3cb698ba6fc352
StagnantBytes <artyom.shevchenko2005@gmail.com>
2025-05-12 18:35:34 +0200

    Revert "testing the shorter stay of hitboc for ninja wave"
    This reverts commit fef465498491c99c066c85f3a45c0eee3f8b609b.

--- src/gameObjects/Character.js
+++ src/gameObjects/Character.js
@@ -959,7 +959,7 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
                         crescent.setStrokeStyle(3, 0x00ffff);
                         
                         // Destroy the crescent hitbox after a short duration
-                        this.scene.time.delayedCall(80, () => {
+                        this.scene.time.delayedCall(2000, () => {
                             if (crescent) {
                                 crescent.destroy();
                             }
