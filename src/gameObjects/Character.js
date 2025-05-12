26e5b132e3b81a6f4d96e2dc6a0e1b61be05f8b4
StagnantBytes <artyom.shevchenko2005@gmail.com>
2025-05-12 06:52:13 +0200

    okay i relized rhat i didnt initilize th array

--- src/gameObjects/Character.js
+++ src/gameObjects/Character.js
@@ -746,6 +746,8 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
     } 
     //Skeleton fireball special attack
     createFireball(positions = null) {
+        // Initialize fireballs array
+        this.fireballs = []; 
         if (!this.fireball) {
             console.log('fireball has been called');
             
@@ -778,6 +780,7 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
                 this.fireball.body.setSize(20, 30);
                 this.fireball.body.setAllowGravity(false);
                 this.scene.fireballs.add(this.fireball);
+                this.fireballs.push(fireball); // Add to our tracking array
             }
             // Set velocity based on configuration
             this.scene.fireballs.children.each((fireball) => {
@@ -814,6 +817,14 @@ export class Character extends Phaser.Physics.Arcade.Sprite {
             if (this === this.scene.gameSync?.localPlayer) {
                 this.scene.networkManager.sendFireballDestroyed({ id: this.scene.networkManager.playerId });
             }
+            
+            // Destroy all fireballs in the array
+            this.fireballs.forEach(fireball => {
+                if (fireball && fireball.active) {
+                    console.log(`${this.characterType} fireball destroyed at x=${fireball.x}, y=${fireball.y}`);
+                    fireball.destroy();
+                }
+            });
             
             // Clear the array
             this.fireballs = null;
