fde5947549225ffb0e57669975c6ea3455c49f56
StagnantBytes <artyom.shevchenko2005@gmail.com>
2025-05-12 18:35:16 +0200

    Revert "making hero overpowerd"
    This reverts commit 8d7c5c44fbdb7d212dc22ce15cc8c41c0f182e78.

--- src/gameObjects/HeroCharacter.js
+++ src/gameObjects/HeroCharacter.js
@@ -15,15 +15,15 @@ export class HeroCharacter extends Character {
             attackDamage: 30,       // Normal attack damage
             attack2Damage: 40,      // Shockwave damage
             attack1Cooldown: 500, // 0.5 seconds mele attack (alredy defualt value)
-            attack2Cooldown: 1000, // 2 seconds cooldown for attack2 
+            attack2Cooldown: 2000, // 2 seconds cooldown for attack2 
 
             //projectile modifiers such as speed and lastability
             projectileVelocity: 250,     // Herowave speed (was 250)
-            projectileLifetime: 500,     // Herowave lifetime (was 300ms)
+            projectileLifetime: 300,     // Herowave lifetime (was 300ms)
             
             //movementspeed modifiers
-            moveSpeed: 300,        
-            jumpVelocity: 500,             
+            moveSpeed: 200,        
+            jumpVelocity: 480,             
 
             //miscellaneous modifiers 
             invincibilityDuration: 800, 
