5c0b7b789a0cc760d9266f678893b7fc30ed913c
StagnantBytes <artyom.shevchenko2005@gmail.com>
2025-05-14 09:32:17 +0200

    might be beacuse i was exliding th layerjoin and left events

--- src/multiplayer/NetworkManager.js
+++ src/multiplayer/NetworkManager.js
@@ -9,7 +9,7 @@ export default class NetworkManager {
 
     this.currentScene = null;
     this.sceneSpecificEvents = {
-      'Lobby': ['lobby_status_update', 'player_ready_state', 'gameJoined'],
+      'Lobby': ['lobby_status_update', 'player_ready_state', 'gameJoined', 'playerJoined', 'playerLeft'],
       'Game': ['playerUpdated', 'playerJoined', 'playerLeft', 'playerHealthUpdate']
     };
   }
